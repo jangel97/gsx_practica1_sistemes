14fbe363d2a2682a12dcece96b41ecb0eb9cc4de
joseangel.morena <joseangel.morena@estudiants.urv.cat>
2019-02-06 12:50:32 +0100

    gpgp.sh (exercici 1) modificat

--- gpgp.sh
+++ gpgp.sh
@@ -5,7 +5,7 @@
 
 if [[ -e "$1" && -f "$1" ]]; then
 	for fitxer in $(cat "$1");do
-		if [[ -e $fitxer && -f $fitxer ]]; then
+		if [ -e $fitxer ]; then
 			stat -c "%U %G %a %n" $fitxer | tee -a ./permissions.txt
 		else
 			echo "Error Fitxer $fitxer no existeix o es tracta de un directori" >&2
