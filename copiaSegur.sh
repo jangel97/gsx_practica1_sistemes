ae32c280017ea897d2152bb78be217d1f95350fc
Joan Jara Bosch <joan.jara@estudiants.urv.cat>
2019-03-02 14:15:27 +0100

    funciona

--- copiaSegur.sh
+++ copiaSegur.sh
@@ -14,20 +14,13 @@ for directori in "$directoris";do
 done
 dataDir=$(date +"%y%m%d%H%M")
 mkdir -p /back/$dataDir
-
+directoriBackUp="/back/$dataDir/"
 arrancada=$(who -r | tr - " " | awk '{ print substr($4,3,2)$5$6}') 
 arrancada=$(date --date=$arrancada +%s)
 avui=$(date +%s)
 dies=$((($avui - $arrancada) / (60*60*24*7)))
-fitxers=$(find / -type f -mtime "$dies" 2>/dev/null)
-IFS=$'\n'
-for file in $fitxers
-do
-	echo "$file" >> /back/"$dataDir"/gpInformation.txt
-done
-unset IFS
-cd /back/$dataDir/
-/usr/bin/gpgp.sh gpInformation.txt > /dev/null
-readlink -f $(ls) >> gpInformation.txt
-tar -cvf "$dataDir.tgz" -T gpInformation.txt
-rm /back/"$dataDir"/*.txt
+find / -type f -mtime "$dies" 2>/dev/null > $directoriBackUp"gpInformation.txt"
+/usr/bin/gpgp.sh $directoriBackUp"gpInformation.txt" 
+tar -czpvf $directoriBackUp"$dataDir.tgz" $directoriBackUp"gpInformation.txt" "/permissions.txt" -T $directoriBackUp"gpInformation.txt"
+rm "$directoriBackUp""gpInformation.txt"
+rm "/permissions.txt" 
